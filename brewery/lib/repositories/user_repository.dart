afdc7895b4140cf8c89f83daf981f167ad567306
Marcin Kazmierski <m.kazmierski@expans.io>
2021-04-06 11:26:12 +0200

    UserRepository: added fake logout and register.

--- brewery/lib/repositories/user_repository.dart
+++ brewery/lib/repositories/user_repository.dart
@@ -1,12 +1,26 @@
 import 'package:brewery/models/user.dart';
 
 abstract class UserRepository {
-  User login(String login, String password);
+  User login(String email, String password);
+
+  bool register(String email, String nick, String password);
+
+  bool logout();
 }
 
 class FakeUserRepository extends UserRepository {
   @override
-  User login(String login, String password) {
-    return new User(id: 1, email: login, nick: "TestowyNick");
+  User login(String email, String password) {
+    return new User(id: 1, email: email, nick: "TestowyNick");
+  }
+
+  @override
+  bool logout() {
+    return true;
+  }
+
+  @override
+  bool register(String email, String nick, String password) {
+    return true;
   }
 }
