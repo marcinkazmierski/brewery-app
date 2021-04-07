b3e84b693bd2c15b3e84ebc5261cfc170df3b6d0
Marcin Kazmierski <m.kazmierski@expans.io>
2021-04-07 18:56:19 +0200

    review list on details page.

--- brewery/lib/screens/details/components/body.dart
+++ brewery/lib/screens/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:brewery/constants.dart';
 import 'package:brewery/models/beer.dart';
+import 'package:flutter_svg/svg.dart';
 import 'backdrop_rating.dart';
 import 'genres.dart';
 import 'title_duration_and_fav_btn.dart';
@@ -80,8 +81,122 @@ class Body extends StatelessWidget {
             ),
           ),
           SizedBox(height: kDefaultPadding),
+          //todo: osobny komponent:
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: kDefaultPadding / 2,
+              horizontal: kDefaultPadding,
+            ),
+            child: Text(
+              "Recenzje",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: kDefaultPadding,
+            ),
+            child: ListLayout(),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ListLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: [
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/icons/star_fill.svg",
+                  height: 16,
+                ),
+                SizedBox(height: kDefaultPadding / 4),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: "5.5/",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      TextSpan(text: "10\n"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            title: Text('Adam'),
+            subtitle: Text('Wszystko fajnie! Polecam!'),
+          ),
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/icons/star_fill.svg",
+                  height: 16,
+                ),
+                SizedBox(height: kDefaultPadding / 4),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: "6.5/",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      TextSpan(text: "10\n"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            title: Text('Janusz'),
+            subtitle: Text('Wszystko fajnie! Polecam!'),
+          ),
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/icons/star_fill.svg",
+                  height: 16,
+                ),
+                SizedBox(height: kDefaultPadding / 4),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: "7.5/",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      TextSpan(text: "10\n"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            title: Text('Staś'),
+            subtitle: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+          ),
+        ],
+      ).toList(),
     );
   }
 }
